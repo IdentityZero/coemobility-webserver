13f4a76cd99c0888c18f634a554ba4113fe42808
IdentityZero <lovelycadiente1@gmail.com>
2024-03-30 16:34:47 +0800

    Added CORS

--- parking_website/parking_website/settings.py
+++ parking_website/parking_website/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    # 'corsheaders',
+    'corsheaders',
 
     "core",
     "Users",
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -134,7 +135,7 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Static files (CSS, JavaScript, Images)
