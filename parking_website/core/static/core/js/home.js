fcbd5ded9c71dc274ff743c7b859f8b7df7aabb1
IdentityZero <lovelycadiente1@gmail.com>
2024-03-30 13:40:46 +0800

    Using public IP

--- parking_website/core/static/core/js/home.js
+++ parking_website/core/static/core/js/home.js
@@ -86,8 +86,7 @@ function formatDate(date){
     return date.toLocaleDateString('en-US', options);
 }
 
-// const dataServerDomainName = 'http://122.248.192.233'
-const dataServerDomainName = 'http://localhost:8000'
+const dataServerDomainName = 'http://122.248.192.233'
 
 function setCoveredParkingSpace(area, area_id, state){
     var parkingSpaceContainer = document.getElementById(`${area}-${area_id}`)
