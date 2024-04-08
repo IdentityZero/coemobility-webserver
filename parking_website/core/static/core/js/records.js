3670c18a90aba1237326d6f164d4e93f41e22aae
IdentityZero <lovelycadiente1@gmail.com>
2024-04-08 14:34:34 +0800

    Using IP address of the server for debugging

--- parking_website/core/static/core/js/records.js
+++ parking_website/core/static/core/js/records.js
@@ -29,7 +29,7 @@ async function fetchData(url, maxRetries = 3, retryDelay = 1000) {
 }
 
 // const dataServerDomainName = 'http://localhost'
-const dataServerDomainName = ''
+const dataServerDomainName = 'http://47.129.54.22:8000'
 const userID = document.getElementById("user-id").value
 const parkingRecordsURL = `${dataServerDomainName}/webapi/parking/records/${userID}/all/`;
 
