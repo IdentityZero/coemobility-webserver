04667f1f3f98f5dd6c23cae4864e6b3652192dae
IdentityZero <lovelycadiente1@gmail.com>
2024-04-29 14:48:53 +0800

    Updated admin for vehicles

--- parking_website/Vehicles/admin.py
+++ parking_website/Vehicles/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import VehiclesVehiclecategory,VehiclesVehicleclassification,VehiclesVehicles,VehiclesVehiclerfid
+
 # Register your models here.
+class VehicleCategory(admin.ModelAdmin):
+    list_display = ['vehicle_category']
+admin.site.register(VehiclesVehiclecategory)
+
+class VehicleClassificationColumn(admin.ModelAdmin):
+    list_display = ['vehicle_classification', 'vehicle_category']
+admin.site.register(VehiclesVehicleclassification, VehicleClassificationColumn)
+
+class VehiclesColumn(admin.ModelAdmin):
+    list_display = ['vehicle_owner', 'vehicle_plate_number', 'vehicle_classification']
+admin.site.register(VehiclesVehicles, VehiclesColumn)
+
+class VehicleRFIDColumn(admin.ModelAdmin):
+    list_display = ['vehicle', 'vehicle_rfid']
+admin.site.register(VehiclesVehiclerfid, VehicleRFIDColumn)
