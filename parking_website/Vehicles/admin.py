64d64e34734f968f51a3b8a9648ee1673778df9d
IdentityZero <lovelycadiente1@gmail.com>
2024-04-30 20:06:49 +0800

    Fixed error in Vehicle/admin

--- parking_website/Vehicles/admin.py
+++ parking_website/Vehicles/admin.py
@@ -17,18 +17,4 @@ admin.site.register(VehiclesVehicles, VehiclesColumn)
 
 class VehicleRFIDColumn(admin.ModelAdmin):
     list_display = ['vehicle', 'vehicle_rfid']
-admin.site.register(VehiclesVehiclerfid, VehicleRFIDColumn)class VehicleCategoryColumn(admin.ModelAdmin):
-    list_display = ['vehicle_category']
-admin.site.register(VehiclesVehiclecategory,VehicleCategoryColumn)
-
-class VehicleClassificationColumn(admin.ModelAdmin):
-    list_display = ['vehicle_classification', 'vehicle_category']
-admin.site.register(VehiclesVehicleclassification, VehicleClassificationColumn)
-
-class VehiclesColumn(admin.ModelAdmin):
-    list_display = ['vehicle_owner', 'vehicle_plate_number', 'vehicle_classification']
-admin.site.register(VehiclesVehicles, VehiclesColumn)
-
-class VehicleRFIDColumn(admin.ModelAdmin):
-    list_display = ['vehicle', 'vehicle_rfid']
 admin.site.register(VehiclesVehiclerfid, VehicleRFIDColumn)
