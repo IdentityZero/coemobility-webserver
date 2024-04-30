d976364f27eb0aea3e497afbd54909af1624cdcd
IdentityZero <lovelycadiente1@gmail.com>
2024-04-30 19:43:42 +0800

    Updated admin names

--- parking_website/Users/admin.py
+++ parking_website/Users/admin.py
@@ -2,9 +2,24 @@ from django.contrib import admin
 from .models import UsersDepartment, AuthUser,UsersProfile, UsersIdentity
 
 # Register your models here.
-admin.site.register(UsersDepartment)
-admin.site.register(UsersIdentity)
-admin.site.register(AuthUser)
-admin.site.register(UsersProfile)
+class UsersDepartmentColumn(admin.ModelAdmin):
+    list_display = ['dept_abbr', 'dept_name']
+admin.site.register(UsersDepartment, UsersDepartmentColumn)
+
+
+class UsersIdentityColumn(admin.ModelAdmin):
+    list_display = ['identity_name']
+admin.site.register(UsersIdentity, UsersIdentityColumn)
+
+
+class UsersIdentityColumn(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name']
+admin.site.register(AuthUser,UsersIdentityColumn )
+
+
+class UsersIdentityColumn(admin.ModelAdmin):
+    list_display = ['user', 'user_department', 'user_identity', 'user_university_number', 'user_contact_number']
+admin.site.register(UsersProfile,UsersIdentityColumn)
+
 
 admin.site.site_header = 'Coemobility Administration'
