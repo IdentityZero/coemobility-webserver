3165eef7b47794fd73bd26e1e9ed80b019851e15
IdentityZero <lovelycadiente1@gmail.com>
2024-05-02 23:00:41 +0800

    moved static root and media

--- parking_website/parking_website/settings/base.py
+++ parking_website/parking_website/settings/base.py
@@ -112,6 +112,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- parking_website/parking_website/settings/dev.py
+++ parking_website/parking_website/settings/dev.py
@@ -17,11 +17,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost',
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "/media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = "/static/"
 
 DATA_SERVER_URL = 'http://localhost'
 
